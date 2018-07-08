c39f87b96c36e2d0b96f631819a624587db83e5c
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2018-07-08 14:28:12 -0700

    Convert backend into a Database class.

--- bookstore_gui.py
+++ bookstore_gui.py
@@ -41,8 +41,9 @@ ______________________________________________________
 """
 
 from tkinter import *
-import backend
+from backend import Database
 
+database = Database("books.db")
 
 def get_selected_row(event):
     global selected_tuple
@@ -63,29 +64,29 @@ def get_selected_row(event):
 
 def view_command():
     list1.delete(0, END)
-    for row in backend.view():
+    for row in database.view():
         list1.insert(END, row)
 
 def search_command():
     list1.delete(0, END)
     # title_text is a StringVar object, not a string.  we use get() to 
     # convert stringvar to a string
-    for row in backend.search(title_text.get(), author_text.get(),
+    for row in database.search(title_text.get(), author_text.get(),
             year_text.get(), isbn_text.get()):
         list1.insert(END, row)
 
 def add_command():
-    backend.insert(title_text.get(), author_text.get(),
+    database.insert(title_text.get(), author_text.get(),
             year_text.get(), isbn_text.get())
     list1.delete(0, END)
     list1.insert(END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
 
 def delete_command():
-    backend.delete(selected_tuple[0])
+    database.delete(selected_tuple[0])
     view_command()
 
 def update_command():
-    backend.update(selected_tuple[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    database.update(selected_tuple[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
     view_command()
 
 MY_FONT = ("Veranda", 10)
